60c1306d4eb12eddfb36d5d09586c7007dce599f
silgon <silgon3200@gmail.com>
2013-04-23 22:54:25 +0200

    deleted all the php snippets and nxhtml mode reactivated, it's heavier but not as bad as multi-web-mode

--- init.el
+++ init.el
@@ -76,24 +76,24 @@
 (global-set-key (kbd "M-<f8>") 'flyspell-check-next-highlighted-word)
 
 ;; MUMAMO
-;; (load "~/.emacs.d/nxhtml/autostart.el")
-;; (setq mumamo-background-colors nil)
+(load "~/.emacs.d/nxhtml/autostart.el")
+(setq mumamo-background-colors nil)
 
 ;; tabkey2 it seems it's really problematic, I deactivated
 ;;(tabkey2-mode t)
 
-;; php mode
-(require 'php-mode)
+;; ;; php mode
+;; (require 'php-mode)
 
-;; multi web mode
-(add-to-list 'load-path "~/.emacs.d/elisp/multi-web-mode/")
-(require 'multi-web-mode)
-(setq mweb-default-major-mode 'html-mode)
-(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-					 (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-					 (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("php" "ctp" "htm" "html" "ctp" "phtml" "php4" "php5"))
-(multi-web-global-mode 1)
+;; ;; multi web mode
+;; (add-to-list 'load-path "~/.emacs.d/elisp/multi-web-mode/")
+;; (require 'multi-web-mode)
+;; (setq mweb-default-major-mode 'html-mode)
+;; (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+;; 					 (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+;; 					 (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+;; (setq mweb-filename-extensions '("php" "ctp" "htm" "html" "ctp" "phtml" "php4" "php5"))
+;; (multi-web-global-mode 1)
 
 ;; obsolete stuff in mumamo
 (when (and (equal emacs-major-version 23)
@@ -236,8 +236,8 @@
 
 
 ;; aliases
-(defalias 'html-mode 'nxml-mode)
-(defalias 'xml-mode 'nxml-mode)
+;; (defalias 'html-mode 'nxml-mode)
+;; (defalias 'xml-mode 'nxml-mode)
 
 ;; major modes
 
@@ -259,7 +259,7 @@
 	(cons '("\\.m$" . octave-mode) auto-mode-alist))
 
 ;;cakephp
-(add-to-list 'auto-mode-alist '("\\.ctp$" . php-mode)) 
+(add-to-list 'auto-mode-alist '("\\.ctp$" . nxhtml-mumamo-mode)) 
 ;; html
 (add-to-list 'auto-mode-alist '("\\.html$" . nxml-mode)) 
 
