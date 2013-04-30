feb19a8874dbaf463eda609bcf4c6617b5764761
silgon <silgon3200@gmail.com>
2013-04-30 18:31:08 +0200

    test for mail xP

--- init.el
+++ init.el
@@ -299,3 +299,18 @@
 ;; html
 (add-to-list 'auto-mode-alist '("\\.html$" . nxml-mode)) 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(send-mail-function (quote smtpmail-send-it))
+ '(spice-output-local "Gnucap")
+ '(spice-simulator "Gnucap")
+ '(spice-waveform-viewer "Gwave"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
