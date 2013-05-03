5ae7bde8aaa828da1f7ad3b4ec8cf714c3c6bd66
silgon <silgon3200@gmail.com>
2013-05-03 15:16:51 +0200

    octave-comment-start

--- init.el
+++ init.el
@@ -313,7 +313,8 @@ or nil if not found."
 (add-to-list 'auto-mode-alist '("\\.launch$" . nxml-mode)) ;;cakephp
 
 ;;octave m files
-(autoload 'octave-mode "octave-mod" nil t)
+(autoload 'octave-mode "octave-mode" nil t)
+(setq-default octave-comment-start "% ")
 (setq auto-mode-alist
 	(cons '("\\.m$" . octave-mode) auto-mode-alist))
 
