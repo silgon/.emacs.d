829281e3d6b6f478d6d7c03f7ebf36bc005f39c1
silgon <silgon3200@gmail.com>
2014-04-10 14:12:08 +0200

    Org-mode updated to export to markdown; and markdown-mode added Errors: - markdownmode doesn't accept scroll down/up by one - org-mode sometimes doesn't display export markdown

--- init.el
+++ init.el
@@ -35,6 +35,17 @@
 (load "auctex.el" nil t t)
 (load "preview-latex.el" nil t t)
 
+;; org-mode
+(add-to-list 'load-path "~/.emacs.d/elisp/org-mode/lisp")
+
+;; markdown-mode
+(add-to-list 'load-path "~/.emacs.d/elisp/markdown-mode")
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
 ;; show parent
 (show-paren-mode t)
 
