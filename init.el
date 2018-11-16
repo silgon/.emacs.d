6a08f24269bba1b8e107a42f2f042a4a7f422c94
silgon <silgon3200@gmail.com>
2018-11-16 23:24:43 +0100

    update|python-docstrings: shortcuts

--- init.el
+++ init.el
@@ -272,8 +272,8 @@
     (local-set-key (kbd "C-. C-h") 'hide-entry) ;; not working well
     (local-set-key (kbd "C-. C-a") 'show-all)  ;; not necessary because it's in global already
     (local-set-key (kbd "C-. C-l") 'hide-body-recenter)
-    (local-set-key (kbd "C-. C-c") 'hide-python-docstrings) ;; not working well
-    (local-set-key (kbd "C-. C-n") 'show-python-docstrings) ;; not working well
+    (local-set-key (kbd "C-. C-d") 'hide-python-docstrings) ;; not working well
+    (local-set-key (kbd "C-. d") 'show-python-docstrings) ;; not working well
 
     )
 (add-hook 'python-mode-hook 'hs-python-and-shortcuts)
